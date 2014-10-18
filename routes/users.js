dc9b7a384f1968142ebde852ae97632f2e3ecf61
Jack Conger <mail@jpconger.com>
2014-10-18 08:23:34 -0700

    updating API

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var UserSchema = require('../models/user');
+var User = mongoose.model('User', UserSchema, 'User');
 
+// SPEC: /users/:user_id/
+// 		returns user info in format
+// 		{
+// 			name:
+// 			phone:
+// 			email:
+// 			groups:
+// 			preferences:
+// 		}
 
+router.get('/:id', function(req, res) {
+	User.findOne({ 'fbid': req.params.id }, function (err, user) {
+		res.json({ 'user' : user });
+	});
+});
 
 module.exports = router;
