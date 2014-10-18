a8f75383424d62b353dfcb03b18f54cd5a18b3c5
Mallika Mathur <mkmathur@cs.uw.edu>
2014-10-18 08:41:57 -0700

    user route

--- routes/users.js
+++ routes/users.js
@@ -6,13 +6,7 @@ var User = mongoose.model('User', UserSchema, 'User');
 
 // SPEC: /users/:user_id/
 // 		returns user info in format
-// 		{
-// 			name:
-// 			phone:
-// 			email:
-// 			groups:
-// 			preferences:
-// 		}
+// 		{"user":{"_id":"54427d0f27d5e1000086d8ac","name":"Mallika Mathur","fbid":"10152402158866724","__v":0,"groups":[]}}
 
 router.get('/:id', function(req, res) {
 	console.log(req.params.id);
@@ -20,5 +14,7 @@ router.get('/:id', function(req, res) {
 		res.json({ 'user' : u });
 	});
 });
+
+
 
 module.exports = router;
