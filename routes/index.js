1544f713e19e3d11f97f4fddcf874ec3ffb977d1
Mallika Mathur <mkmathur@cs.uw.edu>
2014-10-17 23:14:35 -0700

    yay facebook login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,56 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+// PASSPORT
+var passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+    clientID: 1533343480240465,
+    clientSecret: "7b3a7a55257dd4f8e9e36067b7d56ab1",
+    callbackURL: "http://0.0.0.0:3000/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    done(null, profile);
+  }
+));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  res.send('respond with a resource');
+});
+
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+router.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+router.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { successRedirect: '/success',
+                                      failureRedirect: '/' }));
+
+router.get('/success', function(req, res) {
+	res.send('success!');
 });
 
 module.exports = router;
