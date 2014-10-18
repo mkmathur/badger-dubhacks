92a1aa35eace71163808369b38f21471b9ca4f7b
Jack Conger <mail@jpconger.com>
2014-10-18 08:58:13 -0700

    groups & poops

--- routes/groups.js
+++ routes/groups.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var GroupSchema = require('../models/group');
 var Group = mongoose.model('Group', GroupSchema, 'Group');
+var UserSchema = require('../models/user');
+var User = mongoose.model('User', UserSchema, 'User');
 
 // SPECCCCCC
 // GET /:group_id/
@@ -13,18 +15,66 @@ var Group = mongoose.model('Group', GroupSchema, 'Group');
 //     members: list of String ids
 //     tasks: list of int ids
 // }
+
+router.get('/:id', function(req, res) {
+	u = req.user;
+	id = req.params.id;
+	Group.findById(id, function (err, g) {
+		if (err) res.send(err);
+		else res.json({ 'group' : g });
+	});
+});
+
 // POST /groups/
 // requires: logged in
 // Form needed:
 //     name: the name of the group
 // makes group with given name
-//
+
+router.post('/', function(req, res) {
+	grp = new Group();
+	grp.name = req.body.name;
+	grp.save(function(err) {
+		if(err) res.send(err);
+		else res.send('response' : 'group created');
+	});
+});
+
+// POST /groups/:group_id/:user_id
+//     adds the user to the group
+
+router.post('/:gid/:uid', function(req, res) {
+	Group.findById(req.body.gid, function(err, g) {
+		if (err) res.send(err);
+		else {
+			g.members.push(req.body.uid);
+			g.save(function(err) {
+				if(err) res.send(err);
+				else res.json({ 'group' : g });
+			});
+		}
+	});
+	User.findById(req.body.uid, function(err, u) {
+		if (err) res.send(err);
+		else {
+			u.members.push(req.body.gid);
+			u.save(function(err) {
+				if(err) res.send(err);
+				else res.json({ 'user' : u });
+			});
+		}
+	});
+
+});
+
 // POST /groups/group_id/task
 // give me a form with
 //     name: "do the dishes"
 //     nextDue: Date (REQUIRES date in future)
 //     recurrence: "DAILY"
 // and i'll create a task in the given group and return a taskID
+
+
 
 /*
 router.get('/:id', function(req, res) {
